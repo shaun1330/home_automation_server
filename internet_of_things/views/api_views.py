fe1793d783d9bf36e95bd9d8a3806d3537a4a478
Shaun Simons <shaunsimons93@gmail.com>
2025-03-08 09:52:44 +1100

    type hints for request arg in views

--- internet_of_things/views/api_views.py
+++ internet_of_things/views/api_views.py
@@ -1,19 +1,18 @@
 from rest_framework.views import APIView
-
 from internet_of_things.models import Device, DeviceLog
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.request import Request
 from internet_of_things.serializer import DeviceSerializer, DeviceLogSerializer
 
-
 @api_view(['GET'])
-def api_get_devices(request):
+def api_get_devices(request: Request):
     devices = Device.objects.all()
     serializer = DeviceSerializer(devices, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
-def api_get_device(request, device_id: int):
+def api_get_device(request: Request, device_id: int):
     device = Device.objects.get(id=device_id)
     serializer = DeviceSerializer(device, many=False)
     return Response(serializer.data)
@@ -21,7 +20,7 @@ def api_get_device(request, device_id: int):
 
 class DeviceLogAPIView(APIView):
     @staticmethod
-    def get(request, device_id: int):
+    def get(request: Request, device_id: int):
         start_date = request.GET.get('start_date')
         end_date = request.GET.get('end_date')
         device_logs = DeviceLog.objects.filter(device_id=device_id)
@@ -33,7 +32,8 @@ class DeviceLogAPIView(APIView):
         return Response(serializer.data)
 
     @staticmethod
-    def post(request, device_id: int):
+    def post(request: Request, device_id: int):
+        print(type(request), "post request type")
         data = request.data
         device_log =  DeviceLog.objects.create(device_id=device_id, log=data)
         serializer = DeviceLogSerializer(device_log, many=False)
--- internet_of_things/views/views.py
+++ internet_of_things/views/views.py
@@ -3,29 +3,29 @@ from django.db import transaction
 from internet_of_things.models import Device, DeviceLog, DeviceLogField
 from django.shortcuts import render, redirect
 from internet_of_things.forms import DeviceForm
+from django.core.handlers.wsgi import WSGIRequest
 
-def home(request):
+def home(request: WSGIRequest):
+    print(type(request))
     return render(request, 'home.html')
 
-def devices_list(request):
+def devices_list(request: WSGIRequest):
     devices = Device.objects.all()
     context = {"devices": devices}
     return render(request, 'all_devices.html', context=context)
 
-def device_details(request, device_id: int):
+def device_details(request: WSGIRequest, device_id: int):
     device = Device.objects.get(id=device_id)
     context = {"device": device}
     return render(request, 'device_details.html', context=context)
 
-def device_logs(request, device_id: int):
+def device_logs(request: WSGIRequest, device_id: int):
     logs = DeviceLog.objects.filter(device_id=device_id)
     log_fields = DeviceLogField.objects.filter(device_id=device_id).order_by('sort_order')
     context = {"logs": logs, "log_fields": log_fields}
     return render(request, 'device_logs.html', context=context)
 
-
-def create_device(request):
-
+def create_device(request: WSGIRequest):
     if request.method == 'GET':
         form = DeviceForm()
         return render(request, 'create_device.html', {'form': form})
